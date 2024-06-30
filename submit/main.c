b29252ab2611af4c937f23f9d66cfc9c340d0876
tookuyam <tookuyam@42.student.fr>
2024-06-30 14:39:33 +0900

    add: for check arguments

--- submit/main.c
+++ submit/main.c
@@ -6,16 +6,24 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 12:57:36 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/30 13:11:01 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/30 14:38:22 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "px.h"
+#include "libft.h"
+
+void	px_print_help(void);
 
 int	main(int argc, char *argv[])
 {
 	int	result;
 
+	if (argc < 5)
+	{
+		px_print_help();
+		return (1);
+	}
 	result = px_main(argc, argv);
 	if (result != 0)
 	{
@@ -23,4 +31,17 @@ int	main(int argc, char *argv[])
 		return (1);
 	}
 	return (0);
+}
+
+void	px_print_help(void)
+{
+	ft_putstr_fd(
+		"USAGE\n"
+		" pipex file1 cmd1 cmd2... file2\n"
+		" pipex here_doc DELIMINATOR cmd1 cmd2... file3\n"
+		"\n"
+		" file1 : input file\n"
+		" file2 : output file which old file is truncated\n"
+		" file3 : output file. write to end for appending\n"
+		" cmd   : command\n", 1);
 }
