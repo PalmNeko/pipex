23f197eed210e6d97a744d1c6f457f12c3801699
tookuyam <tookuyam@42.student.fr>
2024-06-30 13:17:30 +0900

    fix: declare

--- submit/px.h
+++ submit/px.h
@@ -6,14 +6,14 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 12:57:51 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/30 13:10:02 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/30 13:16:59 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PX_H
 # define PX_H
 
-char	*px_here_doc(int fd, char *deliminator);
+char	*px_store_memory_from_input(int fd, const char *deliminator);
 int		px_main(int argc, char *argv[]);
 int		px_pipex_from_file(int argc, char *argv[]);
 int		px_pipex_here_doc(int argc, char *argv[]);
