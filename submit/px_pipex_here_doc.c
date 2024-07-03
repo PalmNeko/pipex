1001a3daa4dca9819bbdfb4da957f870d52ea130
tookuyam <tookuyam@42.student.fr>
2024-07-03 16:18:44 +0900

    update: return status for here_doc

--- submit/px_pipex_here_doc.c
+++ submit/px_pipex_here_doc.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@42.student.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 13:13:43 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/03 13:50:22 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/03 16:01:23 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,49 @@
 #include "px_types.h"
 
 t_f_px_fork	px_return_fork_for_here_doc(int index, int argc);
+pid_t		px_pipe_for_here_doc(int argc, char *argv[]);
 
 int	px_pipex_here_doc(int argc, char *argv[])
+{
+	int		exit_status;
+	pid_t	last_pid;
+
+	last_pid = px_pipe_for_here_doc(argc, argv);
+	if (last_pid == -1)
+		return (-1);
+	exit_status = px_wait_termed(argc - 2, last_pid);
+	if (exit_status == -1)
+		return (-1);
+	return (exit_status);
+}
+
+pid_t	px_pipe_for_here_doc(int argc, char *argv[])
 {
 	int			index;
 	int			pre_fds[2];
 	int			now_fds[2];
-	t_f_px_fork	f_fork;
+	pid_t		last_pid;
+	pid_t		pid;
 
-	if (argc < 6)
-		return (PX_EARGCNT);
 	if (pipe(pre_fds) == -1)
 		return (-1);
 	index = 2;
+	last_pid = -1;
 	while (index < argc)
 	{
 		if (pipe(now_fds) == -1)
-			return (-1);
-		f_fork = px_return_fork_for_here_doc(index, argc);
-		if (f_fork == NULL || f_fork(argv[index], pre_fds, now_fds) == -1
-			|| px_close_pipe(pre_fds) == -1)
+			return (px_close_pipe(pre_fds), -1);
+		pid = px_return_fork_for_here_doc(index, argc)(
+				argv[index], pre_fds, now_fds);
+		if (index == argc - 2)
+			last_pid = pid;
+		if (pid == -1 || px_close_pipe(pre_fds) == -1)
 			return (-1);
 		ft_memmove(pre_fds, now_fds, sizeof(int) * 2);
 		index++;
 	}
-	if (px_wait_termed(argc - 1) == -1 || px_close_pipe(pre_fds) == -1)
-		return (-1);
-	return (0);
+	px_close_pipe(pre_fds);
+	return (last_pid);
 }
 
 /**
