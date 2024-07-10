a9ba4e79828430ec9e7fdf5f81b5bd8bda8df823
tookuyam <tookuyam@42.student.fr>
2024-07-10 16:01:30 +0900

    fix: gen

--- submit/px_gen_pcmd_for_file.c
+++ submit/px_gen_pcmd_for_file.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   px_gen_pcmd_for_heredoc.c                          :+:      :+:    :+:   */
+/*   px_gen_pcmd_for_file.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/10 14:40:10 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/10 16:01:13 by tookuyam         ###   ########.fr       */
+/*   Created: 2024/07/10 15:27:29 by tookuyam          #+#    #+#             */
+/*   Updated: 2024/07/10 16:00:26 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,25 @@
 #include <errno.h>
 #include <stdlib.h>
 
-t_px_pipe_cmd	**px_gen_pcmd_for_heredoc(int argc, char *argv[])
+t_px_pipe_cmd	**px_gen_pcmd_for_file(int argc, char *argv[])
 {
 	t_px_pipe_cmd	**pipe_cmds;
-	const int		others_argc = 4;
+	const int		others_argc = 3;
 	int				index;
 	int				size;
 
-	if (argc < 6)
+	if (argc < 5)
 		return (ft_set_errno(ERANGE), NULL);
 	size = (argc - others_argc + 1);
 	pipe_cmds = (t_px_pipe_cmd **)malloc(sizeof(t_px_pipe_cmd *) * size);
 	if (pipe_cmds == NULL)
 		return (NULL);
 	ft_memset(pipe_cmds, 0, sizeof(t_px_pipe_cmd *) * size);
-	index = 3;
+	index = 2;
 	while (index < argc - 1)
 	{
-		pipe_cmds[index - 3] = _px_gen_pipe_cmd_for_heredoc(index, argc, argv);
-		if (pipe_cmds[index - 3] == NULL)
+		pipe_cmds[index - 2] = _px_gen_pipe_cmd_for_heredoc(index, argc, argv);
+		if (pipe_cmds[index - 2] == NULL)
 			return (px_free_pcmd_array(pipe_cmds), NULL);
 		index++;
 	}
@@ -44,13 +44,8 @@ t_px_pipe_cmd	*_px_gen_pipe_cmd_for_heredoc(int index, int argc, char *argv[])
 {
 	int	here_fd;
 
-	if (index == 3)
-	{
-		here_fd = px_generate_heredoc_unlinked_fd(argv[index - 1]);
-		if (here_fd == -1)
-			return (NULL);
-		return (px_new_pcmd(NULL, NULL, argv[index], here_fd));
-	}
+	if (index == 2)
+		return (px_new_pcmd(argv[index - 1], NULL, argv[index], here_fd));
 	else if (index < argc - 2)
 		return (px_new_pcmd(NULL, NULL, argv[index], -1));
 	else
