4fc027e8305acf1cea1f264aeb92a4068347f499
tookuyam <tookuyam@42.student.fr>
2024-07-10 17:15:06 +0900

    fix: bugs? for double free

--- submit/px_free_pcmd_array.c
+++ submit/px_free_pcmd_array.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 15:21:54 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/10 15:28:19 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/10 17:09:43 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ void	px_free_pcmd_array(t_px_pipe_cmd **pipe_cmds)
 
 	top_ptr = pipe_cmds;
 	while (*pipe_cmds != NULL)
+	{
 		px_destroy_pcmd(*pipe_cmds);
+		pipe_cmds++;
+	}
 	free(top_ptr);
 	return ;
 }
--- submit/px_main.c
+++ submit/px_main.c
@@ -6,13 +6,14 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 13:11:34 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/10 16:47:59 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/10 17:06:33 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "px.h"
 #include "px_errors.h"
 #include "libft.h"
+#include <stdio.h>
 
 int	px_main(int argc, char *argv[])
 {
@@ -28,6 +29,8 @@ int	px_main(int argc, char *argv[])
 	if (cmds == NULL)
 		return (1);
 	result = px_fork_all_pcmd(cmds);
+	perror(NULL);
+	px_free_errinfo();
 	px_free_pcmd_array(cmds);
 	return (result);
 }
