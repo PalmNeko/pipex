ce816edd0d4b0799c9c15b3db807bf9067d3f220
tookuyam <tookuyam@42.student.fr>
2024-07-11 19:35:01 +0900

    update: for sorted

--- submit/px.h
+++ submit/px.h
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 12:57:51 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/11 19:32:21 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/11 19:33:35 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,31 @@
 # include <sys/types.h>
 # include "px_types.h"
 
-int				px_main(int argc, char *argv[]);
-void			px_print_error(int px_main_error);
+bool			px_is_not_exists(int eno);
+bool			px_is_permission_error(int eno);
+char			*px_get_errinfo(void);
+char			*px_resolve_command_path(char *command);
 int				px_close_pipe(int pipe[2]);
-void			px_errexit_child(void);
-void			px_perrinfo(int status);
-void			px_perror_cmd_not_found(char *info);
-void			px_destroy_pcmd(t_px_pipe_cmd *pipe_cmd);
+int				px_execve(t_px_pipe_cmd *pipe_cmd);
+int				px_generate_heredoc_unlinked_fd(char *deliminator);
+int				px_get_error(void);
+int				px_int_wait_termed(int child_cnt, pid_t last_pid);
+int				px_main(int argc, char *argv[]);
+int				px_run_all_pcmd(t_px_pipe_cmd **pipe_cmd);
+int				px_set_errinfo(char *errinfo);
+pid_t			px_fork_for_pipe(t_px_pipe_cmd *pipe_cmd, int in_pipe[2]);
+t_px_pipe_cmd	**px_gen_pcmd_for_file(int argc, char *argv[]);
+t_px_pipe_cmd	**px_gen_pcmd_for_heredoc(int argc, char *argv[]);
 t_px_pipe_cmd	*px_new_pcmd(
 					char *infile, char *outfile, char *command, int here_fd);
-t_px_pipe_cmd	**px_gen_pcmd_for_heredoc(int argc, char *argv[]);
-t_px_pipe_cmd	**px_gen_pcmd_for_file(int argc, char *argv[]);
-void			px_free_pcmd_array(t_px_pipe_cmd **pipe_cmds);
-int				px_generate_heredoc_unlinked_fd(char *deliminator);
-void			px_free_errinfo(void);
-char			*px_get_errinfo(void);
-int				px_set_errinfo(char *errinfo);
-int				px_execve(t_px_pipe_cmd *pipe_cmd);
-int				px_int_wait_termed(int child_cnt, pid_t last_pid);
-int				px_run_all_pcmd(t_px_pipe_cmd **pipe_cmd);
-char			*px_resolve_command_path(char *command);
 void			free_termed_null(char **mems);
-pid_t			px_fork_for_pipe(t_px_pipe_cmd *pipe_cmd, int in_pipe[2]);
-int				px_get_error(void);
+void			px_destroy_pcmd(t_px_pipe_cmd *pipe_cmd);
+void			px_errexit_child(void);
+void			px_free_errinfo(void);
+void			px_free_pcmd_array(t_px_pipe_cmd **pipe_cmds);
+void			px_perrinfo(int status);
+void			px_perror_cmd_not_found(char *info);
+void			px_print_error(int px_main_error);
 void			px_set_error(int errno);
-bool			px_is_permission_error(int eno);
-bool			px_is_not_exists(int eno);
 
 #endif
