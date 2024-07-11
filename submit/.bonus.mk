e6fe7ec10dd529ea504088db72b0d11026cf7735
tookuyam <tookuyam@42.student.fr>
2024-07-11 20:23:04 +0900

    add: for libft, bonus, clean rule, fclean rule and ignore .out

--- submit/.bonus.mk
+++ submit/.bonus.mk
@@ -1,13 +1,37 @@
 
 NAME          = bonus
 SRCDIR        = .
-EXCLUDE_PATH  = "*/libft/*.c" "*/cident/*" "*/test/*"
-SRCS          = $(shell find $(SRCDIR) -name "*.c" $(addprefix -not -path, $(EXCLUDE_PATH)) -type f)
+SRCS          = \
+	./px_print_error.c \
+	./px_perrinfo.c \
+	./px_perror_cmd_not_found.c \
+	./px_errexit_child.c \
+	./px_wait_termed.c \
+	./px_destroy_pcmd.c \
+	./px_new_pcmd.c \
+	./px_generate_heredoc_unlinked_fd.c \
+	./px_gen_pcmd_for_heredoc.c \
+	./px_free_pcmd_array.c \
+	./px_gen_pcmd_for_file.c \
+	./px_errinfo.c \
+	./px_execve.c \
+	./px_resolve_command_path.c \
+	./free_termed_null.c \
+	./px_close_pipe.c \
+	./px_fork_for_pipe.c \
+	./main.c \
+	./px_fork_all_pcmd.c \
+	./px_main.c \
+	./px_error.c \
+	./px_is_permission_error.c \
+	./px_is_not_exists.c
 OBJS          = $(SRCS:%.c=%.o)
 DEPENDS       = $(OBJS:.o=.d)
-CFLAGS        += -Wall -Wextra -Werror -MMD -MP -O3 $(addprefix -I,$(LIBS))
+CFLAGS        += -Wall -Wextra -Werror -MMD -MP $(addprefix -I,$(LIBS))
 LDFLAGS       += $(addprefix -L,$(LIBS))
 LDLIBS        += -lft
+LIBFT         = libft
+LIBS          = $(LIBFT)
 
 all: $(NAME)
 
@@ -37,13 +61,8 @@ debug: re
 $(NAME): $(LIBFT) $(OBJS)
 	$(LINK.o) $(OBJS) $(LOADLIBES) $(LDLIBS) -o $@
 
-bonus:
-	make -C .bonus.mk
-
 $(LIBFT):
-	bash -c "$(curl https://raw.githubusercontent.com/PalmNeko/Libft/main/install.sh)"
-
-LIBFT         = libft
-LIBS          = $(LIBFT)
+	git clone git@github.com:PalmNeko/Libft.git $@
+	cd $@ && make
 
 -include $(DEPENDS)
